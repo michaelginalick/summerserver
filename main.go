d890a47654dc424020304ed21c4eadd4b5a5decb
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-06-10 18:35:35 -0500

    appending events into a linked list

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import(
 
 const link = "https://www.choosechicago.com/events-and-shows/festivals-guide/"
 
+
+type event struct {
+	name string
+	link string
+	date string
+	next *event
+}
+
+
 func scrapeEventPage() {
   // Request the HTML page.
 	res, err := http.Get(link)
@@ -16,7 +25,8 @@ func scrapeEventPage() {
   if err != nil {
     log.Fatal(err)
   }
-  defer res.Body.Close()
+	defer res.Body.Close()
+	
   if res.StatusCode != 200 {
     log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
   }
@@ -26,16 +36,36 @@ func scrapeEventPage() {
   if err != nil {
     log.Fatal(err)
   }
+	eventList := &event{}
 
   // Find the events
   doc.Find("h3").Each(func(i int, s *goquery.Selection) {
-		event := s.Find("a").Text()
+		name := s.Find("a").Text()
 		link, _ := s.Find("a").Attr("href")
 		date := s.Text()
-    fmt.Printf("%s - %s - %s\n", event, date, link)
-  })
+
+		newEvent := &event{name, link, date, nil }
+
+		eventList = addBeginning(newEvent, eventList)
+	})
+	
+	res.Body.Close()
+
+	printList(eventList)
 }
 
 func main() {
   scrapeEventPage()
+}
+
+func addBeginning(newEvent, eventList *event) *event {
+	newEvent.next = eventList
+	return newEvent
+}
+
+
+func printList(eventList *event) {
+	for i := eventList; i != nil; i = i.next {
+		fmt.Println(i.name, i.date)
+	}
 }

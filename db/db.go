3d6009efa1446ca952bf80e034e434f6b5f4dc1e
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-06-16 21:38:11 -0500

    saving records to database, added helper functions to check if records exists

--- db/db.go
+++ db/db.go
@@ -1,9 +1,12 @@
 package db
 
-import(
+import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	event "../structs"
+	"github.com/lib/pq"
 )
 
 const (
@@ -14,21 +17,59 @@ const (
 	dbname   = "summerserver"
 )
 
-
-
-// OpenDB :  opens a connection to a database
+// OpenDB :  opens a connection to the database
 func OpenDB() *sql.DB {
-  var db *sql.DB
+	var db *sql.DB
 
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-  db, err := sql.Open("postgres", psqlInfo)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
+
+// SaveRecords :  saves event records to the database
+func SaveRecords(e *event.Event) {
+	db := OpenDB()
+
+	defer db.Close()
+
+	for i := e; i != nil; i = i.Next {
+    if(!isPresent(i, db)) {
+		  _, err := db.Exec(
+        "INSERT INTO  events (name, link, month, days, individual_days, festival_length) VALUES ($1, $2, $3, $4, $5, $6)",
+        i.Name, i.Link, i.Month, pq.Array(i.Days), pq.Array(i.IndividualDays), i.FestivalLength,
+      )
+      checkErr(err)
+    }
+	}
+	db.Close()
+}
+
+
+func isPresent(e *event.Event, db  *sql.DB) (exists bool) {
+
+  sqlStatement := `select exists(select 1 from events where name = $1);`
+  rows, err := db.Query(sqlStatement, e.Name)
+  
+  checkErr(err)
+
+  for rows.Next() {
+    err := rows.Scan(&exists)
+    checkErr(err)
+  }
+  return exists
+}
+
+
+func checkErr(err error) {
   if err != nil {
     panic(err)
   }
-  
-  if err = db.Ping(); err != nil {
-    log.Fatal(err)
-  }
-  
-  return db
 }
--- db/summerServer.sql
+++ db/summerServer.sql
@@ -1,20 +1,16 @@
 DROP DATABASE IF EXISTS "summerserver";
 CREATE DATABASE "summerserver";
 
-USE "summerserver";
-
-
-
-CREATE TABLE "Events" (
-	"id" serial NOT NULL,
-	"name" VARCHAR(255) NOT NULL,
-	"link" VARCHAR(255) NOT NULL,
-	"month" VARCHAR(255),
-	"days" integer,
-	"individual_days" integer,
-	"festival_length" integer,
-	CONSTRAINT Events_pk PRIMARY KEY ("id")
-)
+CREATE TABLE events (
+	id serial NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	link VARCHAR(255) NOT NULL,
+	month VARCHAR(255),
+	days VARCHAR(255),
+	individual_days VARCHAR(255),
+	festival_length VARCHAR(255),
+	CONSTRAINT Events_pk PRIMARY KEY (id)
+);
 
 
 

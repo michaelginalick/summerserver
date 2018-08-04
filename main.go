d6422181cd66337aa08be1f380a04045af8a7f3f
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-08-03 20:31:41 -0500

    rename routes

--- main.go
+++ main.go
@@ -13,9 +13,8 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/events", eventsController.GetEvents).Methods("GET")
-
 	router.HandleFunc("/events/{id}", eventsController.GetEventByID).Methods("GET")
-	router.HandleFunc("/eventsByMonth/{month}", eventsController.GetEventsByMonth).Methods("GET")
+	router.HandleFunc("/events_by_month/{month}", eventsController.GetEventsByMonth).Methods("GET")
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET"})
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods)(router)))

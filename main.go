5b87149d891204e7aa2897e71492fd4710d86c89
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2019-02-12 17:18:38 -0600

    add post route sqash case bug

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ func main() {
 	router.HandleFunc("/events", eventscontroller.GetEvents).Methods("GET")
 	router.HandleFunc("/events/{id}", eventscontroller.GetEventByID).Methods("GET")
 	router.HandleFunc("/events_by_month/{month}/{year}", eventscontroller.GetEventsByMonth).Methods("GET")
+	router.HandleFunc("/create_event", eventscontroller.CreateEvent).Methods("POST")
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
-	allowedMethods := handlers.AllowedMethods([]string{"GET"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST"})
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods)(router)))
 }
